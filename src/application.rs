3e52109431039bae0345393d6c66185d8980b6cf
DaPigGuy <mcpepig123@gmail.com>
2024-06-04 23:58:00 -0700

    Port to `Adw.AboutDialog`

--- src/application.rs
+++ src/application.rs
@@ -3,6 +3,7 @@
 
 use std::{cell::RefCell, rc::Rc};
 
+use adw::prelude::AdwDialogExt;
 use adw::subclass::prelude::*;
 #[cfg(any(target_os = "linux", target_os = "freebsd"))]
 use ashpd::{desktop::background::Background, WindowIdentifier};
@@ -229,8 +230,7 @@ impl Application {
 
     fn show_about(&self) {
         let window = self.active_window().unwrap();
-        let dialog = adw::AboutWindow::builder()
-            .transient_for(&window)
+        let dialog = adw::AboutDialog::builder()
             .application_icon(APPLICATION_ID)
             .application_name("Amberol")
             .developer_name("Emmanuele Bassi")
@@ -244,7 +244,7 @@ impl Application {
             .translator_credits(i18n("translator-credits"))
             .build();
 
-        dialog.present();
+        dialog.present(Some(&window));
     }
 
     #[cfg(any(target_os = "linux", target_os = "freebsd"))]
