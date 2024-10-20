b8b4f29319097aa124783836f2942ca31aaf10d0
Emmanuele Bassi <ebassi@gnome.org>
2024-10-20 17:26:00 +0100

    Remove tool installation from check.sh
    The tools should already be installed.

--- build-aux/checks.sh
+++ build-aux/checks.sh
@@ -17,7 +17,6 @@ If a dependency is not found, helps the user to install it.
 USAGE: ${0##*/} [OPTIONS]
 
 OPTIONS:
-    -f, --force-install     Install missing dependencies without asking
     -t, --tap               Use TAP output
     -v, --verbose           Use verbose output
     -h, --help              Display this help and exit
@@ -45,7 +44,6 @@ ok="${pos}ok${res}"
 fail="${neg}fail${res}"
 
 # Initialize variables
-force_install=0
 verbose=0
 tap=0
 
@@ -88,120 +86,15 @@ diff() {
     done
 }
 
-# Check if rustup is available.
-# Argument:
-#   '-i' to install if missing.
-check_rustup() {
-    if ! which rustup &> /dev/null; then
-        if [[ "$1" == '-i' ]]; then
-            echo -e "$Installing rustup…"
-            curl https://sh.rustup.rs -sSf  | sh -s -- -y --default-toolchain nightly
-            export PATH=$PATH:$HOME/.cargo/bin
-            if ! which rustup &> /dev/null; then
-                echo -e "$Failed to install rustup"
-                exit 2
-            fi
-        else
-            exit 2
-        fi
-    fi
-}
-
-# Install cargo via rustup.
-install_cargo() {
-    check_rustup -i
-    if ! which cargo >/dev/null 2>&1; then
-        echo -e "$Failed to install cargo"
-        exit 2
-    fi
-}
-
-# Check if cargo is available. If not, ask to install it.
-check_cargo() {
-    if ! which cargo >/dev/null 2>&1; then
-        echo "Unable to find cargo"
-
-        if [[ $force_install -eq 1 ]]; then
-            install_cargo
-        elif [ ! -t 1 ]; then
-            exit 2
-        elif check_rustup; then
-            echo -e "$error rustup is installed but the cargo command isn't available"
-            exit 2
-        else
-            echo ""
-            echo "y: Install cargo via rustup"
-            echo "N: Don't install cargo and abort checks"
-            echo ""
-            while true; do
-                echo -n "Install cargo? [y/N]: "; read yn < /dev/tty
-                case $yn in
-                    [Yy]* )
-                        install_cargo
-                        break
-                        ;;
-                    [Nn]* | "" )
-                        exit 2
-                        ;;
-                    * )
-                        echo $invalid
-                        ;;
-                esac
-            done
-        fi
-    fi
-
-    if [[ $verbose -eq 1 ]]; then
-        echo ""
-        rustc -Vv && cargo +nightly -Vv
-    fi
-}
-
-# Install rustfmt with rustup.
-install_rustfmt() {
-    check_rustup -i
-
-    echo -e "$Installing rustfmt…"
-    rustup component add --toolchain nightly rustfmt
-    if ! cargo +nightly fmt --version >/dev/null 2>&1; then
-        echo -e "$Failed to install rustfmt"
-        exit 2
-    fi
-}
-
 # Run rustfmt to enforce code style.
 run_rustfmt() {
     if ! cargo fmt --version >/dev/null 2>&1; then
         if [[ $tap -eq 1 ]]; then
             echo "ok 1 - rustfmt # SKIP Not installed"
             return
-        fi
-        if [[ $force_install -eq 1 ]]; then
-            install_rustfmt
-        elif [ ! -t 1 ]; then
+        else
             echo "Unable to check the project's code style, because rustfmt could not be run"
             exit 2
-        else
-            echo "Rustfmt is needed to check the project's code style, but it isn’t available"
-            echo ""
-            echo "y: Install rustfmt via rustup"
-            echo "N: Don't install rustfmt and abort checks"
-            echo ""
-            while true; do
-                echo -n "Install rustfmt? [y/N]: "; read yn < /dev/tty
-                case $yn in
-                    [Yy]* )
-                        install_rustfmt
-                        break
-                        ;;
-                    [Nn]* | "" )
-                        exit 2
-                        ;;
-                    * )
-                        echo $invalid
-                        ;;
-                esac
-            done
         fi
     fi
 
@@ -232,49 +125,15 @@ run_rustfmt() {
 }
 
 
-# Install typos with cargo.
-install_typos() {
-    echo -e "$Installing typos…"
-    cargo install typos-cli
-    if ! typos --version >/dev/null 2>&1; then
-        echo -e "$Failed to install typos"
-        exit 2
-    fi
-}
-
 # Run typos to check for spelling mistakes.
 run_typos() {
     if ! typos --version >/dev/null 2>&1; then
         if [[ $tap -eq 1 ]]; then
             echo "ok 2 - typos # SKIP Not installed"
             return
-        fi
-        if [[ $force_install -eq 1 ]]; then
-            install_typos
-        elif [ ! -t 1 ]; then
+        else
             echo "Unable to check spelling mistakes, because typos could not be run"
             exit 2
-        else
-            echo "Typos is needed to check spelling mistakes, but it isn’t available"
-            echo ""
-            echo "y: Install typos via cargo"
-            echo "N: Don't install typos and abort checks"
-            echo ""
-            while true; do
-                echo -n "Install typos? [y/N]: "; read yn < /dev/tty
-                case $yn in
-                    [Yy]* )
-                        install_typos
-                        break
-                        ;;
-                    [Nn]* | "" )
-                        exit 2
-                        ;;
-                    * )
-                        echo $invalid
-                        ;;
-                esac
-            done
         fi
     fi
 
@@ -504,8 +363,6 @@ esac; shift; done
 [ $tap == 1 ] && echo "TAP version 14"
 [ $tap == 1 ] && echo "1..4"
 
-check_cargo
-[ $tap == 0 ] && echo ""
 run_rustfmt
 [ $tap == 0 ] && echo ""
 run_typos
